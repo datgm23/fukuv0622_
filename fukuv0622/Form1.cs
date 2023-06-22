15d85875ee8199a3fa6d059230fcc689b7f6b422
YuTanaka <tanakaedu@users.noreply.github.com>
2023-06-22 13:22:59 +0900

    完成

--- fukuv0622/Form1.cs
+++ fukuv0622/Form1.cs
@@ -2,9 +2,27 @@ namespace fukuv0622
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] data = new int[10];
+            label1.Text = "";
+            int sum = 0;
+
+            for (int i=0;i<10;i++)
+            {
+                data[i] = rand.Next(101);
+                label1.Text += $"{data[i]}, ";
+                sum += data[i];
+            }
+
+            MessageBox.Show($"{sum/10}");
         }
     }
 }
